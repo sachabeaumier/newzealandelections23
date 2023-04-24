c1eadf1cdb8f27836a653a1a2332967a66d60219
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-04-24 22:35:43 +1200

    routes and db files worked on

---
+++ server/routes/donations.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import { Donation } from '../../client/common/donation'
+import { getAllDonations, getOneDonation } from '../db/db'
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  getAllDonations()
+    .then((donations) => {
+      res.json(donations)
+    })
+    .catch((err: Error) => {
+      res.status(500).send(err.message)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  getOneDonation(Number(req.params.id))
+    .then((donation) => res.json(donation))
+    .catch((err) => res.status(500).json({ status: 500, error: err.message }))
+})
+
+export default router
