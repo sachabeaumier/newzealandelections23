e499b5002b777b003de0bd5cf65d30fd07d409db
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-05-01 18:21:44 +1200

    last changes

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,76 +1,21 @@
-// // import Donationundertwentyk from './SingleDonationUnderTwentyk'
+// import Donationundertwentyk from './SingleDonationUnderTwentyk'
 // import DonationsUnderTwentyKo from './DonationsUnderTwentyk'
 // import { fetchAllDonationsUnderTwenty } from '../actions/donations'
-// import { useAppDispatch } from '../hooks/redux'
-// import { useEffect } from 'react'
-// import { NavLink, Route, Routes } from 'react-router-dom'
-// // import Home from './Home'
-
-// function App() {
-//   const dispatch = useAppDispatch()
-
-//   useEffect(() => {
-//     dispatch(fetchAllDonationsUnderTwenty())
-//   }, [])
-
-//   return (
-//     <>
-//       <div>
-//         <h1>New Zealand Political Party Donors Tracker API</h1>
-//         <header className="header">
-//           <h1>New Zealand Political Party Donors Tracker API</h1>
-//         </header>
-//         <section className="main">
-//           <h1>New Zealand Political Party Donors Tracker API</h1>
-
-//           {/* <Home /> */}
-//           {/* add your code here */}
-//           <Routes>
-//             <Route path="/" element={<DonationsUnderTwentyKo />} />
-//           </Routes>
-//         </section>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default App
-////////////////////////////////////
-
-// import DonationUnderTwentyK from './SingleDonationUnderTwentyK'
-import DonationsUnderTwentyK from './DonationsUnderTwentyK'
-import { fetchAllDonationsUnderTwenty } from '../actions/donations'
 import { useAppDispatch } from '../hooks/redux'
 import { useEffect } from 'react'
 import { NavLink, Route, Routes } from 'react-router-dom'
-import Home from './Home'
+// import Home from './Home'
 
 function App() {
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchAllDonationsUnderTwenty())
-  }, [])
-
   return (
     <>
-      <div>
-        <h1>New Zealand Political Party Donors Tracker API</h1>
-        <header className="header">
-          <h1>New Zealand Political Party Donors Tracker API</h1>
-        </header>
-        <section className="main">
-          <h1>New Zealand Political Party Donors Tracker API</h1>
-
-          {/* <Home /> */}
-          {/* add your code here */}
-          <Routes>
-            <Route path="/" element={<DonationsUnderTwentyK />} />
-          </Routes>
-        </section>
-      </div>
+      <header className="header">
+        <h1>My Collection</h1>
+      </header>
+      <section className="main">{/* add your code here */}</section>
     </>
   )
 }
 
 export default App
+////////////////////////////////////
